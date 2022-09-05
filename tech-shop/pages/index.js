fbfa7347ac4fa1a6bdffb20eee101519c2adaf92
Magdalena Perry <magperry16@gmail.com>
2022-09-05 11:41:49 -0400

    update css page from tutorial

--- tech-shop/pages/index.js
+++ tech-shop/pages/index.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Home   = () => {
   return (
-    <div>Home</div>
-  )
+<>
+HeroBanner
+<div>
+  <h2>Best Selling Products</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatibus totam ab labore ipsa dicta harum commodi vitae cupiditate adipisci ducimus inventore fugiat, voluptas esse molestias omnis mollitia minima assumenda.</p>
+</div>
+
+<div>
+  {["item1", "item2"].map((product) => product )}
+</div>
+
+Footer
+
+</>
+    )
 }
 
-export default Home
+export default Home;
