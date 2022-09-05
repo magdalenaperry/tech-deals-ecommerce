b1b682e3f47b9325486dcbc9a6caa248a40fd252
Magdalena Perry <magperry16@gmail.com>
2022-09-05 12:01:28 -0400

    added components in jsx format and updated babel

--- tech-shop/pages/index.js
+++ tech-shop/pages/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 
 const Home   = () => {
   return (
 <>
+
 HeroBanner
-<div>
+
+<div className='products-heading'>
   <h2>Best Selling Products</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatibus totam ab labore ipsa dicta harum commodi vitae cupiditate adipisci ducimus inventore fugiat, voluptas esse molestias omnis mollitia minima assumenda.</p>
 </div>
 
-<div>
+<div className="products-container">
   {["item1", "item2"].map((product) => product )}
 </div>
 
