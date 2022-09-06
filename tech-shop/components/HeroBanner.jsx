890d22024b832efb211e5715283b093aaaf3e358
Magdalena Perry <magperry16@gmail.com>
2022-09-06 09:17:07 -0400

    created client, linked our sanity api & rendered api information to our homepage

--- tech-shop/components/HeroBanner.jsx
+++ tech-shop/components/HeroBanner.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
+      {console.log(heroBanner.smallText)}
       <div>
-        <p className='beats-solo'>small text</p>
-        <h3>mid text</h3>
-        <img src="" alt="headphones" className='hero-banner-image' />
+        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image' />
         
         <div>
-          <Link href="/product/ID">
-            <button type="button">button text</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
-            <h5>description text</h5>
+          <h5>{heroBanner.desc}</h5>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi accusamus eveniet at earum culpa quisquam asperiores fuga quasi! Temporibus nemo necessitatibus eos impedit dignissimos neque sequi doloribus optio tenetur sed!</p>
 
           </div>
--- tech-shop/pages/index.js
+++ tech-shop/pages/index.js
@@ -1,26 +1,42 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components';
 
+import { client } from '../lib/client';
 
-const Home   = () => {
+const Home   = ( {products, bannerData} ) => {
   return (
 <>
 
-<HeroBanner />
-
+<HeroBanner heroBanner={bannerData && bannerData[0]}/>
+    {/* {console.log(products)} */}
+    {/* {console.log(bannerData)} */}
+    
 <div className='products-heading'>
   <h2>Best Selling Products</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatibus totam ab labore ipsa dicta harum commodi vitae cupiditate adipisci ducimus inventore fugiat, voluptas esse molestias omnis mollitia minima assumenda.</p>
 </div>
 
 <div className="products-container">
-  {["item1", "item2"].map((product) => product )}
+  {products?.map((product) => product.name )}
 </div>
 
 <FooterBanner />
 
 </>
     )
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product" ]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner" ]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+
+  }
 }
 
 export default Home;
