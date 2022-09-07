5d400f90717cbe24bba0379091b62296d278c3d7
Magdalena Perry <magperry16@gmail.com>
2022-09-07 08:53:59 -0400

    add to cart with toast banner message addedand functional

--- tech-shop/context/StateContext.js
+++ tech-shop/context/StateContext.js
@@ -7,7 +7,7 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
 
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState([]);
 
   const [totalPrice, setTotalPrice] = useState();
 
@@ -30,6 +30,28 @@ export const StateContext = ({ children }) => {
     const checkProductInCart = cartItems.find(
       (item) => item.id === product._id
     );
+
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
   };
 
   return (
@@ -42,6 +64,7 @@ export const StateContext = ({ children }) => {
         qty,
         incQty,
         decQty,
+        onAdd,
       }}
     >
       {children}
